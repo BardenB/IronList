16b06af4ef08e072357f3e24244dc04e357c0121
Brett Barden <barden.brett@gmail.com>
2025-10-18 21:50:43 -0400

    initial commit

---
+++ iron-list/target/debug/deps/iron_list-63e954e3c0957323.d
@@ -0,0 +1,5 @@
+C:\Users\barde\IronList\iron-list\target\debug\deps\iron_list-63e954e3c0957323.d: src\main.rs
+
+C:\Users\barde\IronList\iron-list\target\debug\deps\libiron_list-63e954e3c0957323.rmeta: src\main.rs
+
+src\main.rs:
---
+++ iron-list/target/debug/iron-list.d
@@ -0,0 +1,1 @@
+C:\Users\barde\IronList\iron-list\target\debug\iron-list.exe: C:\Users\barde\IronList\iron-list\src\main.rs
